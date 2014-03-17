3b2f0bd3572761d5cf1b53cef2f32b0174cfaf94
lpd <lpd@pedromoreira.org>
2014-03-17 15:10:41 +0000

    em principio dados conscan sao suficientes

--- src/ConScan.py
+++ src/ConScan.py
@@ -1,6 +1,7 @@
 import psutil
 import socket
 from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
+from classes import IP, ConScanDB
 
 class ConScan:
     def scan(self):
@@ -14,5 +15,4 @@ class ConScan:
             con = p.get_connections(kind='inet') #or all
             for c in con:
                 if len(c.raddr) > 0:
-                    print "Local Port: %s, Remote IP: %s, Remote Port: %s, Status: %s" % (c.laddr[1], c.raddr[0], c.raddr[1], c.status)
-                    print p
+                    print "Local Port: %s, Remote IP: %s, Remote Port: %s, Status: %s, Name: %s, Pid: %s" % (c.laddr[1], c.raddr[0], c.raddr[1], c.status, p.name(), p.pid)
