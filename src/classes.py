b281295f9d9cf82eef8aaf183365e7dff2f83ae5
lpd <lpd@pedromoreira.org>
2014-03-18 11:57:52 +0000

    inicio da tentativa de crifrar a bd

--- src/classes.py
+++ src/classes.py
@@ -1,8 +1,14 @@
 # http://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/
+#
+# Parte de tentar cifrar / decifrar a bd
+# http://stackoverflow.com/questions/16761458/how-to-aes-encrypt-decrypt-files-using-python-pycrypto-in-an-openssl-compatible
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import hashlib
+from hashlib import md5
+from Crypto.Cipher import AES
+from Crypto import Random
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -39,9 +45,55 @@ class Con:
         self.base = Base
         db = ''.join(chr(ord(a) ^ ord(b))
                      for a,b in zip(username, password))
-        m = hashlib.md5()
+        m = md5()
         m.update(db)
+        p = md5(password)
+        self.psw = p.hexdigest()
         self.hash = m.hexdigest()
         self.db_name = '%s.db' % self.hash
+        with open(self.db_name, 'rb') as in_file, open(self.db_name, 'wb') as out_file:
+            decrypt(in_file, out_file, psw)
         engine = create_engine('sqlite:///%s.db' % self.hash)
         Base.metadata.create_all(engine)
+
+    def derive_key_and_iv(password, salt, key_length, iv_length):
+        d = d_i = ''
+        while len(d) < key_length + iv_length:
+            d_i = md5(d_i + password + salt).digest()
+            d += d_i
+        return d[:key_length], d[key_length:key_length+iv_length]
+
+    def encrypt(in_file, out_file, password, key_length=256):
+        bs = AES.block_size
+        salt = Random.new().read(bs - len('Salted__'))
+        key, iv = derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        out_file.write('Salted__' + salt)
+        finished = False
+        while not finished:
+            chunk = in_file.read(1024 * bs)
+            if len(chunk) == 0 or len(chunk) % bs != 0:
+                padding_length = (bs - len(chunk) % bs) or bs
+                chunk += padding_length * chr(padding_length)
+                finished = True
+            out_file.write(cipher.encrypt(chunk))
+            
+    def decrypt(in_file, out_file, password, key_length=32):
+        bs = AES.block_size
+        salt = in_file.read(bs)[len('Salted__'):]
+        key, iv = derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        next_chunk = ''
+        finished = False
+        while not finished:
+            chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+            if len(next_chunk) == 0:
+                padding_length = ord(chunk[-1])
+                chunk = chunk[:-padding_length]
+                finished = True
+        out_file.write(chunk)
+
+    def close(self):
+        with open(self.db_name, 'rb') as in_file, open(self.db_name, 'wb') as out_file:
+            encrypt(in_file, out_file, self.psw)
+
