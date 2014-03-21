d7a97cc3bdfe6e7b7ceb17d87bc3fe9b21c77007
lpd <lpd@pedromoreira.org>
2014-03-21 13:17:27 +0000

    export para csv feito

--- src/Export.py
+++ src/Export.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from classes import NmapScanDB, ConScanDB, LogScanDB
 from datetime import datetime
+import csv
 
 class Export:
     def __init__(self, db_name, base, filename, filetype):
@@ -30,11 +31,56 @@ class Export:
         pass
 
     def csv(self):
+        filename = "%s.csv" %self.filename
         try:
+            spamWriter = csv.writer(open(filename, 'wb'), delimiter=',',
+                                    quotechar='\x22', quoting=csv.QUOTE_MINIMAL)
+
             conscans = self.session.query(ConScanDB).all()
+            spamWriter.writerow(["LOCAL CONNECTIONS"])
+            spamWriter.writerow(["[Time]"]
+                                + ["[Local Port]"]
+                                + ["[Remote IP]"]
+                                + ["[Remote Port]"])
+            for line in conscans:
+                spamWriter.writerow([line.time]
+                                    + [line.local_port]
+                                    + [line.remote_ip.ip]
+                                    + [line.remote_port])
+
             portscans = self.session.query(NmapScanDB).all()
+            spamWriter.writerow(["PORT SCANS"])
+            spamWriter.writerow(["[Time]"]
+                                + ["[IP]"]
+                                + ["[Protocol]"]
+                                + ["[Port]"])
+            for line in portscans:
+                spamWriter.writerow([line.time]
+                                    + [line.ip.ip]
+                                    + [line.protocol]
+                                    + [line.port])
+
             logscans = self.session.query(LogScanDB).all()
-            print "File saved to %s.csv" % self.filename
+            spamWriter.writerow(["LOG SCANS"])
+            spamWriter.writerow(["[Time]"]
+                                + ["IP"]
+                                + ["[Event]"]
+                                + ["[Device]"]
+                                + ["[Protocol]"]
+                                + ["TTL"]
+                                + ["Src Port"]
+                                + ["Dst Port"])
+            for line in logscans:
+                spamWriter.writerow([line.time]
+                                    + [line.ipsrc.ip]
+                                    + [line.event_src]
+                                    + [line.device]
+                                    + [line.protocol]
+                                    + [line.ttl]
+                                    + [line.src_port]
+                                    + [line.dst_port])
+
+            print "File saved to %s" % filename
         except Exception as e:
             print "Erro: %s" % e
 
