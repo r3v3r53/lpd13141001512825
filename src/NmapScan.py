1bea9de09450531557377f5b380ddbec2d16b867
lpd <lpd@pedromoreira.org>
2014-03-23 14:09:47 +0000

    comentarios

--- src/NmapScan.py
+++ src/NmapScan.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#-*-coding: utf-8-*-
+
 import nmap
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,7 +8,20 @@ from datetime import datetime
 import GeoIP
 
 class NmapScan:
+    '''
+    Classe para o sportscans
+    '''
     def __init__(self, db_name, base, ip, port):
+        '''
+        Constructor
+        Efectua a ligacao a base de dados fornecida
+        e inicia o portscan
+
+        @arg db_name: nome do ficheiro com a base de dados
+        @arg base: alchemy declarative_base()
+        @arg ip: endereco ou range de ips
+        @arg port ou range de ports
+        '''
         self.db_name = db_name
         self.base = base
         self.ip = ip
@@ -20,13 +34,35 @@ class NmapScan:
 
 
     def scan(self):
+        '''
+        Função para efectuar o portscan
+        Grava na base de dados o IP 
+        com a respectiva georeferencia
+        e os resultados obtidos.
+        Simultâneamente os mesmos são impressos na consola
+        '''
         nm = nmap.PortScanner()
         nm.scan(self.ip, self.port)
         now = datetime.now()
         for host in nm.all_hosts():
-            gi = GeoIP.open('GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
-            geo = gi.record_by_addr(host)        
-            new_ip = IP(ip=host, country=geo['country_code'], country_name=geo['country_name'], lon=geo['longitude'], lat=geo['latitude'])
+            try:
+                gi = GeoIP.open('GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
+                geo = gi.record_by_addr(host)
+                country_=geo['country_code']
+                country_name_=geo['country_name']
+                lon_=geo['longitude']
+                lat_=geo['latitude']
+            except:
+                country_ = None
+                country_name_ = None
+                lon_ = None
+                lat_ = None
+            finally:
+                new_ip = IP(ip=host, 
+                        country=country_, 
+                        country_name=country_name_,
+                        lon=lon_, 
+                        lat=lat_)
             #check if ip address is in database
             ip_address = self.session.query(IP).filter_by(ip=host).first()
 
@@ -38,7 +74,7 @@ class NmapScan:
             print('----------------------------------------------------')
             print('Host : %s (%s)' % (host, nm[host].hostname()))
             print('State : %s' % nm[host].state())
-
+            
             for proto in nm[host].all_protocols():
                 if proto in ['tcp', 'udp']:
                     print('----------')
--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # http://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/
 #
 # http://stackoverflow.com/questions/20852664/python-pycrypto-encrypt-decrypt-text-files-with-aes 
@@ -5,7 +6,20 @@
 # https://github.com/maxmind/geoip-api-python
 # apt-get install libgeoip-dev --> https://trac.torproject.org/projects/tor/ticket/10625
 # https://github.com/maxmind/geoip-api-python/blob/master/README.rst
-# http://www.pip-installer.org/en/latest/installing.html#install-or-upgrade-pip 
+# http://www.pip-installer.org/en/latest/installing.html#install-or-upgrade-pip
+'''
+Network Security Application Database classes
+A base de dados é gerada com auxílio do Alchemy
+O nome da base de dados é criada a partir 
+do username e password fornecidos.
+Caso o ficheiro exista, este é decifrado para ser utilizado
+no final das operações deve ser cifrado de novo
+
+@author Pedro Moreira
+@author João Carlos Mendes
+@date 20140323
+
+''' 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -18,6 +32,11 @@ from os import remove
 Base = declarative_base()
 
 class IP(Base):
+    '''
+    Classe para a criação da tabela IP
+    Armazena os endereços ip relacionados 
+    com todos os scans efectuados
+    '''
     __tablename__ = 'ip'
     id = Column(Integer, primary_key=True)
     ip = Column(String(15), nullable=False, unique=True)
@@ -27,6 +46,10 @@ class IP(Base):
     lat = Column(String(50))
 
 class LogScanDB(Base):
+    '''
+    Classe para a criação da tabela logscan
+    Armazena todos os logscan efectuados
+    '''
     __tablename__ = 'logscan'
     id = Column(Integer, primary_key=True)
     time = Column(DateTime)
@@ -42,6 +65,10 @@ class LogScanDB(Base):
     ipsrc = relationship(IP)
     
 class ConScanDB(Base):
+    '''
+    Classe para a criação da tabela conscan
+    Armazena todas as conecções existentes na máquina local
+    '''
     __tablename__ = 'conscan'
     id = Column(Integer, primary_key=True)
     local_port = Column(Integer, nullable=False)
@@ -53,6 +80,10 @@ class ConScanDB(Base):
     remote_ip = relationship(IP)
 
 class NmapScanDB(Base):
+    '''
+    Classe para a criação da tabela portscan
+    Armazena todos os portscan efectuados
+    '''
     __tablename__ = 'portscan'
     id = Column(Integer, primary_key=True)
     port = Column(Integer, nullable=False)
@@ -64,7 +95,20 @@ class NmapScanDB(Base):
     ip = relationship(IP)
 
 class Con:
+    '''
+    Classe para gerir a ligação à base de dados
+    @arg username
+    @arg password
+    '''
     def __init__(self, username, password):
+        '''
+        Constructor:
+        gera o hash md5 do username e da password
+        de seguida é efectuado o XOR entre os dois hashes
+        o resultado desta operação é o nome utilizado
+        para a base de dados do utilizador.
+        A base de dados é então decifrada com a password fornecida
+        '''
         self.deleted = False
         self.base = Base
         u = hashlib.md5()
@@ -73,7 +117,7 @@ class Con:
         p = hashlib.md5()
         p.update(password)
         self.password = p.hexdigest()
-
+        # XOR entre os hash md5 do username e password
         db = ''.join(chr(ord(a) ^ ord(b))
                      for a,b in zip(self.username, self.password))
         m = hashlib.md5()
@@ -89,21 +133,49 @@ class Con:
         Base.metadata.create_all(engine)
 
     def pad(self, s):
+        '''
+        preencher string para ter o tamanho da chave 
+        a utilizar para a cifra
+
+        @arg s: string a converter
+        @return string com o tamanho da chave a utilizar
+        '''
         return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
 
     def encrypt(self, message, key, key_size=256):
+        '''
+        função para cifrar um texto com aes-cbc
+
+        @arg message: texto a cifrar
+        @arg key: chave para cifra
+        @arg key_size: tamanho da cifra (256 por default)
+        @return texto cifrado
+        '''
         message = self.pad(message)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(key, AES.MODE_CBC, iv)
         return iv + cipher.encrypt(message)
 
     def decrypt(self, ciphertext, key):
+        '''
+        função para decifrar um texto com aes-cbc
+
+        @arg message: texto a decifrar
+        @arg key: chave para decifrar
+        @return texto decifrado
+        '''
         iv = ciphertext[:AES.block_size]
         cipher = AES.new(key, AES.MODE_CBC, iv)
         plaintext = cipher.decrypt(ciphertext[AES.block_size:])
         return plaintext.rstrip(b"\0")
     
     def encrypt_file(self, file_name, key):
+        '''
+        função para cifrar um ficheiro com aes-cbc
+
+        @arg file_name: ficheiro a cifrar
+        @arg key: chave para cifrar
+        '''
         with open(file_name, 'rb') as fo:
             plaintext = fo.read()
         enc = self.encrypt(plaintext, key)
@@ -111,6 +183,12 @@ class Con:
             fo.write(enc)
 
     def decrypt_file(self, file_name, key):
+        '''
+        função para decifrar um ficheiro com aes-cbc
+
+        @arg file_name: ficheiro a decifrar
+        @arg key: chave para decifrar
+        '''
         with open(file_name, 'rb') as fo:
             ciphertext = fo.read()
         dec = self.decrypt(ciphertext, key)
@@ -118,11 +196,20 @@ class Con:
             fo.write(dec)        
 
     def close(self):
+        '''
+        função utilizada para cifrar a base de dados,
+        caso esta não tenha sido eliminada
+        Esta função deve ser chamada 
+        no final da execução do programa
+        '''
         if self.deleted == False:
             self.encrypt_file(self.db_name, self.password)
         pass
 
     def delete(self):
+        '''
+        função utilizada para eliminar a base de dados
+        '''
         try:
             remove(self.db_name)
             self.deleted = True
